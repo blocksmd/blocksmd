210e7eeb2c7dc6a725ce50a0845cf962d716375d
halfmoonui <hey.halfmoon@gmail.com>
2024-07-13 19:32:54 +0600

    Bundling

---
+++ types/phone-number-placeholders.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Given a country code, get the correct phone number placeholder.
+ *
+ * @param {string} countryCode
+ * @returns {string}
+ */
+export function getPhoneNumberPlaceholder(countryCode: string): string;
