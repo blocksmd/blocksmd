c01e2023926ac2beae4954038acaac7685d8e932
halfmoonui <hey.halfmoon@gmail.com>
2024-10-31 13:07:31 +0600

    File input bug fix where you could drag in a non-image file and have it be accepted

--- src/index.js
+++ src/index.js
@@ -1185,26 +1185,39 @@ class blocksmd {
 		];
 		const file = e.target.files[0];
 		if (file) {
-			if (imageFileTypes.includes(file.type)) {
-				fileExistsSection.innerHTML = [
-					`<span class="bmd-form-file-img-container">`,
-					`	<img src="${URL.createObjectURL(file)}" alt="${file.name}">`,
-					`</span>`,
-					`<span class="bmd-d-block bmd-mt-3">`,
-					`	<strong class="bmd-text-accent">${file.name}</strong>`,
-					`</span>\n`,
-				].join("\n");
-			} else {
-				fileExistsSection.innerHTML = [
-					`<span class="bmd-form-file-img-container">`,
-					`	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="bmd-icon" aria-hidden="true" focusable="false"><path d="M352 448l0-256-112 0c-26.5 0-48-21.5-48-48l0-112L64 32C46.3 32 32 46.3 32 64l0 384c0 17.7 14.3 32 32 32l256 0c17.7 0 32-14.3 32-32zm-.5-288c-.7-2.8-2.1-5.4-4.2-7.4L231.4 36.7c-2.1-2.1-4.6-3.5-7.4-4.2L224 144c0 8.8 7.2 16 16 16l111.5 0zM0 64C0 28.7 28.7 0 64 0L220.1 0c12.7 0 24.9 5.1 33.9 14.1L369.9 129.9c9 9 14.1 21.2 14.1 33.9L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64z"/></svg>`,
-					`</span>`,
-					`<span class="bmd-d-block bmd-mt-3">`,
-					`	<strong class="bmd-text-accent">${file.name}</strong>`,
-					`</span>\n`,
-				].join("\n");
+			// Bug fix where you can drag in a non-image file and have it be accepted
+			// This bug exists on Chrome for macOS (and maybe others)
+			let fileValid = true;
+			if (
+				e.target.getAttribute("accept") === "image/*" &&
+				!imageFileTypes.includes(file.type)
+			) {
+				e.target.value = "";
+				fileValid = false;
 			}
-			label.classList.add("bmd-file-exists");
+
+			if (fileValid) {
+				if (imageFileTypes.includes(file.type)) {
+					fileExistsSection.innerHTML = [
+						`<span class="bmd-form-file-img-container">`,
+						`	<img src="${URL.createObjectURL(file)}" alt="${file.name}">`,
+						`</span>`,
+						`<span class="bmd-d-block bmd-mt-3">`,
+						`	<strong class="bmd-text-accent">${file.name}</strong>`,
+						`</span>\n`,
+					].join("\n");
+				} else {
+					fileExistsSection.innerHTML = [
+						`<span class="bmd-form-file-img-container">`,
+						`	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="bmd-icon" aria-hidden="true" focusable="false"><path d="M352 448l0-256-112 0c-26.5 0-48-21.5-48-48l0-112L64 32C46.3 32 32 46.3 32 64l0 384c0 17.7 14.3 32 32 32l256 0c17.7 0 32-14.3 32-32zm-.5-288c-.7-2.8-2.1-5.4-4.2-7.4L231.4 36.7c-2.1-2.1-4.6-3.5-7.4-4.2L224 144c0 8.8 7.2 16 16 16l111.5 0zM0 64C0 28.7 28.7 0 64 0L220.1 0c12.7 0 24.9 5.1 33.9 14.1L369.9 129.9c9 9 14.1 21.2 14.1 33.9L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64z"/></svg>`,
+						`</span>`,
+						`<span class="bmd-d-block bmd-mt-3">`,
+						`	<strong class="bmd-text-accent">${file.name}</strong>`,
+						`</span>\n`,
+					].join("\n");
+				}
+				label.classList.add("bmd-file-exists");
+			}
 		}
 	};
 
