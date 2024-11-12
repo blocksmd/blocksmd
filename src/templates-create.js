afec8f8a7597d94b653cc9951c0ab512568008fe
halfmoonui <hey.halfmoon@gmail.com>
2024-11-12 23:28:26 +0600

    Remove square brackets where possible in Nunjucks templates

--- src/templates-create.js
+++ src/templates-create.js
@@ -147,14 +147,14 @@ const bodyTemplate = `
 {% if settings["header-render"] %}
 <header class="bmd-header">
 	<div class="bmd-header-container">
-		{% if settings["brand"] != undefined %}
-		{{ settings["brand"] | safe }}
+		{% if settings.brand != undefined %}
+		{{ settings.brand | safe }}
 		{% if settings["brand-alt-scheme"] != undefined %}
 		{{ settings["brand-alt-scheme"] | safe }}
 		{% endif %}
 		{% endif %}
-		{% if settings["cta"] != undefined %}
-		{{ settings["cta"] | safe }}
+		{% if settings.cta != undefined %}
+		{{ settings.cta | safe }}
 		{% endif %}
 	</div>
 </header>
@@ -183,7 +183,7 @@ const bodyTemplate = `
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="bmd-icon" aria-hidden="true" focusable="false"><path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" /></svg>
 		</button>
 		{% endif %}
-		{% if settings["slide-controls"] != "hide" and settings["page"] != "single" %}
+		{% if settings["slide-controls"] != "hide" and settings.page != "single" %}
 		<div class="bmd-btn-group" role="group">
 			<button type="button" class="bmd-previous-btn bmd-btn bmd-btn-accent bmd-btn-control bmd-btn-control-square bmd-d-flex bmd-align-items-center bmd-justify-content-center" aria-label="{{ translations.previousBtn }}" disabled>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="bmd-icon" aria-hidden="true" focusable="false"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>
